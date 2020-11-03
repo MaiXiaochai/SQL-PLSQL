e10b1a7839e233d70828a1c21f2aa79cdeb7077b
MaiXiaochai <maixiaochai@outlook.com>
2020-11-03 20:44:08 +0800

    +)系统例外，no_data_found,zero_divide

---
+++ src/SCOTT/no_data_found.sql
@@ -0,0 +1,13 @@
+﻿
+set serveroutput on
+declare
+    pename emp.ename%type;
+begin
+    -- 查询一个不存在的员工的员工姓名，员工号为1234;
+    select ename into pename from emp where empno=1234;
+
+exception
+    when no_data_found then dbms_output.put_line('没有查到该员工');
+    when others then dbms_output.put_line('其它例外');
+end;
+/
---
+++ src/SCOTT/zero_divide.sql
@@ -0,0 +1,14 @@
+﻿-- 系统例外：被0除， zero_divide
+set serveroutput on
+
+declare
+    --定义一个基本变量
+    pnum number;
+begin
+    pnum := 1/0;
+    
+exception
+    when zero_divide then dbms_output.put_line('0不能做除数');
+
+end;
+/
