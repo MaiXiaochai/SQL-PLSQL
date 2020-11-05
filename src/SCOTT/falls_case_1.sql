909038a7b4b7fde31365dca1ff6a54b6c7dc5a21
MaiXiaochai <maixiaochai@outlook.com>
2020-11-05 21:42:16 +0800

    +)瀑布设计案例

---
+++ src/SCOTT/falls_case_1.sql
@@ -0,0 +1,37 @@
+﻿--瀑布设计
+set serveroutput on
+
+declare
+    cursor cemp is select to_char(hiredate, 'yyyy') from emp;
+    phiredate varchar2(4);
+    
+    -- 每年入职员工人数
+    count80 number := 0;
+    count81 number := 0;
+    count82 number := 0;
+    count87 number := 0;
+
+begin
+    --打开光标
+    open cemp;
+    loop
+        fetch cemp into phiredate;
+        exit when cemp%notfound;
+        -- 判断入职年份
+        if phiredate = '1980' then count80 := count80 + 1;
+        elsif phiredate = '1981' then count81 := count81 + 1;
+        elsif phiredate = '1982' then count82 := count82 + 1;
+        else count87 := count87 + 1;
+        end if;
+    end loop;
+    --关闭光标
+    close cemp;
+    -- 输出结果
+    dbms_output.put_line('Total: ' || (count80 + count81 + count82 + count87));
+    dbms_output.put_line('1980: ' || count80);
+    dbms_output.put_line('1981: ' || count81);
+    dbms_output.put_line('1982: ' || count82);
+    dbms_output.put_line('1987: ' || count87);
+
+end;
+/
---
+++ src/SCOTT/falls_case_2_add_sal.sql
@@ -0,0 +1,48 @@
+﻿/*  为员工涨工资。
+    从最低工资涨起每人涨10%，但是工资总额不能超过5万块。
+    请计算涨工资的人数和涨工资后的工资总额，
+    并输出涨工资的人数和工资总额
+*/
+set serveroutput on
+
+declare
+    cursor cemp is select empno, sal from emp;
+    pempno emp.empno%type;
+    psal emp.sal%type;
+    
+    --涨工资的人数
+    countEmp number := 0;
+    
+    -- 涨工资后的工资总额
+    salTotal number;
+    
+begin
+    -- 涨工资之前的工资总额
+    select sum(sal) into salTotal from emp;
+
+    open cemp;
+    loop
+        fetch cemp into pempno, psal;
+
+        -- 退出条件1：工资总额 > 5w，退出
+        exit when salTotal + psal * 0.1 > 50000;
+        
+        -- 涨后工资总额
+        salTotal := salTotal + psal * 0.1;
+
+        -- 涨工资
+        update emp set sal = sal * 1.1 where empno = pempno;
+        
+        -- 人数加一
+        countEmp := countEmp + 1;
+        
+        -- 退出条件2：%notfound
+        exit when cemp%notfound;        
+        
+    end loop;
+    
+    --因为有update操作，所以要commit
+    commit;
+    dbms_output.put_line('人数：' || countEmp || '工资总额：' || salTotal);
+end;
+/
